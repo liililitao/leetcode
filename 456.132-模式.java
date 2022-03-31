b2d178f35536804b5622f963f36b93a633efcd04
liubt007 <1449264620@qq.com>
2022-03-31 21:25:41 +0800

    leetcode456

---
+++ 456.132-模式.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=456 lang=java
+ *
+ * [456] 132 模式
+ */
+
+// @lc code=start
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        int n = nums.length;
+        int last = Integer.MIN_VALUE; // 132中的2
+        Stack<Integer> sta = new Stack<>();// 用来存储132中的3
+        if(nums.length < 3)
+            return false;
+        for(int i=n-1; i>=0; i--){
+            if(nums[i] < last) // 若出现132中的1则返回正确值
+                return true;
+            // 若当前值大于或等于2则更新2（2为栈中小于当前值的最大元素）
+            while(!sta.isEmpty() && sta.peek() < nums[i]){
+                last = sta.pop();
+            }
+            // 将当前值压入栈中
+            sta.push(nums[i]);
+        }
+        return false;
+    }
+}
+// @lc code=end
+
