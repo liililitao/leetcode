cb07c771354f6ad4bd2c97cb3709d4e8119653c0
liililitao <1449264620@qq.com>
2022-07-15 17:35:11 +0800

    leetcode687

---
+++ 687.最长同值路径.java
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=687 lang=java
+ *
+ * [687] 最长同值路径
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /*
+     * 令 arrow_length(node) 为从节点 node 延伸出的最长箭头的长度。
+     * 如果 node.Left 存在且与节点 node 具有相同的值，则该值就会是 1 + arrow_length(node.left)。
+     * 在 node.right 存在的情况下也是一样。
+     * 当我们计算箭头长度时，候选答案将是该节点在两个方向上的箭头之和。
+     * 我们将这些候选答案记录下来，并返回最佳答案。
+     */
+    int ans;
+    public int longestUnivaluePath(TreeNode root) {
+        ans = 0;
+        arrowLength(root);
+        return ans;
+    }
+    public int arrowLength(TreeNode node) {
+        if (node == null) return 0;
+        int left = arrowLength(node.left);
+        int right = arrowLength(node.right);
+        int arrowLeft = 0, arrowRight = 0;
+        if (node.left != null && node.left.val == node.val) {
+            arrowLeft += left + 1;
+        }
+        if (node.right != null && node.right.val == node.val) {
+            arrowRight += right + 1;
+        }
+        ans = Math.max(ans, arrowLeft + arrowRight);
+        return Math.max(arrowLeft, arrowRight);
+    }
+}
+// @lc code=end
+
