ef83c8aa0afef370cb3742a67164941071f03913
liililitao <1449264620@qq.com>
2022-08-21 10:14:30 +0800

    leetcode633

---
+++ 633.平方数之和.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=633 lang=java
+ *
+ * [633] 平方数之和
+ */
+
+// @lc code=start
+class Solution {
+    //枚举法
+    public boolean judgeSquareSum(int c) {
+        for (long a = 0; a * a <= c; a++) {
+            double b = Math.sqrt(c - a * a);
+            if (b == (int) b) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+// @lc code=end
+
